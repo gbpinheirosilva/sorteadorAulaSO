0e9dc2e1ab3e5d0357edbfb72dc146eefa55a5b3
gbpinheirosilva <gbpinheirosilva@poli.ufrj.br>
2022-05-31 22:45:35 -0300

    whiping c code

--- randomStudentSelector.c
+++ randomStudentSelector.c
@@ -4,22 +4,7 @@
 
 main(int argv, char *argc[])
 {   
-    int students, questions, number;
-    char option;
-    printf("Enter the number of students: ");
-    scanf("%d", &students);
-    printf("Enter number of questions:\n");
-    scanf("%d", &questions);
     
-    int set1 = [];
-    int set2 = [];
-    int set3 = [];
-    int set4 = [];
-    int absents = [];
-    
-    printf("Pressione N para gerar um novo numero"    )
-    scanf("%c", &option);
-    int number = randomNumber(0, students);
 
 }
 
